8f2a563b5c6ec2bcb8c45372ef3c66885d00bdf3
Samantha <sam.perezf@hotmail.com>
2021-11-24 00:55:46 -0500

    tabla usuario con rol

--- dao/models/usuario.js
+++ dao/models/usuario.js
@@ -14,6 +14,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   Usuario.init({
+    rol: DataTypes.STRING,
     nombre: DataTypes.STRING,
     apellido: DataTypes.STRING,
     DNI: DataTypes.INTEGER,
