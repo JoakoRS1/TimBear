1943ba188aaaa81cde521db55d4314033185a0ab
Joaquin Rodriguez <joaquinrs20181659@gmail.com>
2021-11-24 02:01:12 -0500

    Relacion BD

---
+++ dao/migrations/20211124065318-relacion_Juego_Partida.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('Partida','juegoId',{
+      type : Sequelize.INTEGER,
+      allowNull:true
+    })
+
+    await queryInterface.addConstraint('Partida',{
+      fields : ['juegoId'],
+      type : 'FOREIGN KEY',
+      name : 'FK_PARTIDA_JUEGO',
+      references :{
+        table : 'Juego',
+        field : 'id'
+      }
+
+    })
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeConstraint('Partida','FK_PARTIDA_JUEGO')
+    await queryInterface.removeColumn('Partida', 'juegoId')
+  }
+};
