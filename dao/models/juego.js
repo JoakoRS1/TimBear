aa62d0fe7d969d5bf6e4ce83fb044da74dec4f82
Alessandra Ortega Arrieta <aleortega2000@gmail.com>
2021-11-28 08:21:49 -0500

    relacion juego categoria lista

--- dao/models/juego.js
+++ dao/models/juego.js
@@ -10,7 +10,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // falta definir asociacion con partida !!!
+      Juego.belongsTo(models, Categoria, {
+        foreignKey : 'categoriaId'
+      })
     }
   };
   Juego.init({
